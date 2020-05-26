d3e41a34db6d1cb9d898b06364b0ba5c64a89257
Se Oh <seoh@Ses-MacBook-Pro.local>
2020-05-26 17:41:49 +1000

    260520

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   end
 
   def create 
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user  && user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:email])
+    if user  && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "You have logged in"
       redirect_to users_path(user)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user1 = User.create(username: 'alex', email: 'alex@email.com', password_digest: Password.create('password'))
-user2 = User.create(username: 'brian', email: 'brian@email.com', password_digest: Password.create('password'))
+# user1 = User.create(username: 'alex', email: 'alex@email.com', password_digest: Password.create('password'))
+# user2 = User.create(username: 'brian', email: 'brian@email.com', password_digest: Password.create('password'))
 
-post1 = Post.create(user_id: user1.id, title: 'Hello', content: 'Nice to meet you')
-post2 = Post.create(user_id: user2.id, title: 'Hello', content: 'I am a traveller')
+# post1 = Post.create(user_id: user1.id, title: 'Hello', content: 'Nice to meet you')
+# post2 = Post.create(user_id: user2.id, title: 'Hello', content: 'I am a traveller')
 
-comment1 = Comment.create(user_id: user2.id, post_id: post1.id, title: 'reply', content: 'Nice to meet you too')
-comment2 = Comment.create(user_id: user1.id, post_id: post2.id, title: 'reply', content: 'I am a traveller too')
+# comment1 = Comment.create(user_id: user2.id, post_id: post1.id, title: 'reply', content: 'Nice to meet you too')
+# comment2 = Comment.create(user_id: user1.id, post_id: post2.id, title: 'reply', content: 'I am a traveller too')
