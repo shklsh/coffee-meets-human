4450edc9ad47f935b134e383c8bba2b926741ec7
Se Oh <seoh@Ses-MacBook-Pro.local>
2020-05-26 16:46:28 +1000

    260520

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(ActiveSupport::LoggerSilence)
+Bundler.require(*Rails.groups)
 
 module CoffeeMeetsHuman
   class Application < Rails::Application
